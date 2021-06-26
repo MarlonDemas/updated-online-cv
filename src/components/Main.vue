db6dd078330a29b5445d60d35f9e5039c3a9f9d2
marlondemas <demasmarlon@gmail.com>
2021-06-26 14:23:44 +0200

    added netlify form

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,24 +1,48 @@
 <template>
-    <div class="main main-raised">
-        <div class="section section-basic">
-            <div class="container">
-                <div class="title text-center text-uppercase">
-                    <h2 id="about">About <strong>me</strong></h2>
-                </div>
-                <hr> 
-                <div class="col-md-8 offset-md-2 text-center mt-4" data-aos="fade-up">
-                    I am an ambivert with a very positive attitude and always looking to be challenged. I have the ability to learn concepts quickly and thrive in a fast-paced environment. I am easy to talk to, reliable and very focused.
-                </div>
-                <div class="m-5"></div>
-            </div>
+  <div class="main main-raised">
+    <div class="section section-basic">
+      <div class="container">
+        <div class="title text-center text-uppercase">
+          <h2 id="about">About <strong>me</strong></h2>
         </div>
+        <hr />
+        <div class="col-md-8 offset-md-2 text-center mt-4" data-aos="fade-up">
+          I am an ambivert with a very positive attitude and always looking to
+          be challenged. I have the ability to learn concepts quickly and thrive
+          in a fast-paced environment. I am easy to talk to, reliable and very
+          focused.
+        </div>
+        <div class="m-5"></div>
+      </div>
     </div>
+    <form name="contact" method="POST" data-netlify="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label
+          >Your Role:
+          <select name="role[]" multiple>
+            <option value="leader">Leader</option>
+            <option value="follower">Follower</option>
+          </select></label
+        >
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
@@ -29,7 +53,7 @@ export default {
   padding-top: 75px;
   background: #ccc;
   text-align: center;
-  color: #FFF;
+  color: #fff;
   font-size: 3em;
 }
 </style>
